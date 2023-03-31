20997aca5deaaeaf93acf2d3a65f4e332003d7d6
ingstadshakthi <59392423+ingstadshakthi@users.noreply.github.com>
2023-03-31 12:09:53 +0530

    Add files via upload

---
+++ Day 9 - Sound board/script.js
@@ -0,0 +1,11 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+  const btn = document.createElement("button");
+  btn.classList.add('btn');
+  btn.innerHTML = sound;
+  btn.addEventListener("click", () => {
+    document.getElementById(sound).play();
+  });
+  document.getElementById('buttons').appendChild(btn);
+});
